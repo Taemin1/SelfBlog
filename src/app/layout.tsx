93f183794d2dd1f1d62a92e9197b2466c6f092e8
Taemin1 <rlaxoals132@gmail.com>
2025-01-22 15:32:23 +0900

    폰트 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,25 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import '@/styles/global.css';
 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>SelfBlog</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Header />
         <div className="container">{children}</div>
